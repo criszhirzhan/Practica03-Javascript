9a44c436a668cde5a206e9c5a1dfe8d91e4d0d41
Christian Zhirzhan <czhirzhanc@est.ups.edu.ec>
2019-05-18 19:25:48 -0500

    Se estructura la calculadora

---
+++ js/funciones.js
@@ -0,0 +1,188 @@
+// function saludar(saludo){
+//     alert(saludo)
+// }
+
+// function suma(){
+//     var n1= Number(document.getElementById("numero1").value)
+//     var n2= Number(document.getElementById("numero2").value)
+
+//     var res= n1+n2
+
+//     document.getElementById("resultado").value=res
+
+// }
+
+// function select(ctrl){
+//     console.log(ctrl)
+//     alert("Hola......")
+//     for(i=0;i<ctrl.length;i++)
+//         // if(ctrl[i].checked) return ctrl[i].value;
+//         if (ctrl[0].checked){
+//             suma()
+//         }
+//         // if (ctrl[1].checked){
+//         //     suma()
+//         // }
+//         // if (ctrl[2].checked){
+//         //     suma()
+//         // }
+//         // if (ctrl[3].checked){
+//         //     suma()
+//         // }
+
+// }
+
+//Declaramos variables
+var operandoa;
+var operandob;
+var operacion;
+
+function init() {
+    //variables
+    var resultado = document.getElementById('resultado');
+    var reset = document.getElementById('reset');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('uno');
+    var dos = document.getElementById('dos');
+    var tres = document.getElementById('tres');
+    var cuatro = document.getElementById('cuatro');
+    var cinco = document.getElementById('cinco');
+    var seis = document.getElementById('seis');
+    var siete = document.getElementById('siete');
+    var ocho = document.getElementById('ocho');
+    var nueve = document.getElementById('nueve');
+    var cero = document.getElementById('cero');
+
+}
+
+//Eventos de click
+
+// raiz.onclick = function (e) {
+//     resultado.textContent = resultado.textContent ;
+// }
+
+punto.onclick = function (e) {
+    resultado.textContent = resultado.textContent + ".";
+}
+
+uno.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "1";
+}
+dos.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "2";
+}
+tres.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "3";
+}
+cuatro.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "4";
+}
+cinco.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "5";
+}
+seis.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "6";
+}
+siete.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "7";
+}
+ocho.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "8";
+}
+nueve.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "9";
+}
+cero.onclick = function (e) {
+    resultado.textContent = resultado.textContent + "0";
+}
+reset.onclick = function (e) {
+    resetear();
+}
+suma.onclick = function (e) {
+    operandoa = resultado.textContent;
+    operacion = "+";
+    limpiar();
+}
+resta.onclick = function (e) {
+    operandoa = resultado.textContent;
+    operacion = "-";
+    limpiar();
+}
+multiplicacion.onclick = function (e) {
+    operandoa = resultado.textContent;
+    operacion = "*";
+    limpiar();
+}
+
+raiz.onclick = function (e) {
+    operandoa = resultado.textContent;
+    operacion = "sqrt";
+    limpiar();
+}
+
+elevado2.onclick = function (e) {
+    operandoa=0;
+    operandoa = resultado.textContent;
+    res = Math.pow(parseFloat(operandoa),2);
+    resetear();
+    resultado.textContent = res;
+}
+
+porcentaje.onclick = function (e) {
+    operandoa = resultado.textContent;
+    res = Math.floor(operandoa)/100;
+    resultado.textContent = res;
+    operandoa=res;
+}
+
+division.onclick = function (e) {
+    operandoa = resultado.textContent;
+    operacion = "/";
+    limpiar();
+}
+igual.onclick = function (e) {
+    operandob = resultado.textContent;
+    resolver();
+}
+
+function limpiar() {
+    resultado.textContent = "";
+}
+function resetear() {
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+}
+
+function resolver() {
+    var res = 0;
+    switch (operacion) {
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+            break;
+        case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+            break;
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+            break;
+        case "sqrt":
+            res = Math.sqrt(parseFloat(operandoa));
+            break;
+        // case "pow":
+        //     res = Math.pow(parseFloat(operandoa),2);
+        //     break;
+    }
+    resetear();
+    resultado.textContent = res;
+}
+
+
